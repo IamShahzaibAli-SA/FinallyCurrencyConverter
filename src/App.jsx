777d3aab7b6c810632a8071e03f65836fd2566e6
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-11-26 14:37:10 +0500

    TheConvertingLogicBuild

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,54 @@ import { useState } from 'react'
 import './App.css'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 
+
 function App() {
-  // const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(100);
+  const [currencyFrom, setCurrencyFrom] = useState("usd");
+  const [currencyTo, setCurrencyTo] = useState("pkr");
   
-  let currency = "usd";
-
-  const data = useCurrencyInfo(currency);
-
+  const data = useCurrencyInfo(currencyFrom);
+  
   return (
     <>
       <div className='flex justify-center'>
-        <div className='w-[700px] p-4 bg-gray-400'>
+        <div className='w-[700px] p-4 bg-gray-400 rounded mt-10 text-black'>
           <div>
-            <p>CurrencyConverter</p>
+            <p className='font-semibold text-[24px]'>CurrencyConverter</p>
           </div>
+          <div className='mt-8'>
+            <p>Enter Amount: </p> 
+          </div>
+          <div>
+            <input
+            type="number"
+            className='bg-white'
+            onChange={(e) => setAmount(e.target.value)}
+            />
+          </div>
+
+          <div className='flex justify-between p-4'>
+            <p>Convert From: </p>
+            <p>Convert To: </p>
+          </div>
+
+          <div className='flex justify-between p-4'>
+            <select className='bg-blue-400 px-5 py-2' onChange={(e) => setCurrencyFrom(e.target.value)}>
+              <option value="usd">USD</option>
+              <option value="pkr">PKR</option>
+              <option value="eur">EUR</option>
+            </select>
+            <select className='bg-blue-400 px-5 py-2' onChange={(e) => setCurrencyTo(e.target.value)}>
+              <option value="pkr">PKR</option>
+              <option value="usd">USD</option>
+              <option value="eur">EUR</option>
+            </select>
+          </div>
+
+          <div>
+            <p>{amount }</p>
+          </div>
+          
         </div>
       </div>
     </>
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,18 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency){
-    const fetchCurrency = async () => {
-        const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
-        const response = await fetch(url);
-        const result = await response.json();
+function useCurrencyInfo(currency) {
 
-        console.log(result);
-    }
+    const [data, setData] = useState(null);
 
-    useEffect(()=>{
+    useEffect(() => {
+        const fetchCurrency = async () => {
+            const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
+            const response = await fetch(url);
+            const result = await response.json();
+            setData(result);
+            
+            console.log(result);
+        }
+
         fetchCurrency();
     }, [currency]);
-
+    
+    return data;
 }
 
 export default useCurrencyInfo;
